e2cd1d787dfb9ad91dfde981aecf1b4f0858dd6a
Kirill Matrosov <k.matrosov@orbita.center>
2018-09-24 16:17:58 +0300

    refactor

--- src/routes/not-found/index.js
+++ src/routes/not-found/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from './not-found.styl';
 
 export default function () {
-  return <div>Not Found</div>
+  return <div className={styles.text}>Ошибка 404. Страница не найдена</div>
 }
