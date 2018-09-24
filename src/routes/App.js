d1b160365d81be2f1cc0acbc6bfa5b311684d0bc
Kirill Matrosov <k.matrosov@orbita.center>
2018-09-24 17:19:18 +0300

    fix

--- src/routes/App.js
+++ src/routes/App.js
@@ -7,20 +7,23 @@ import * as userActions from '../store/actions/user';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  state = {};
+
   static getDerivedStateFromProps(nextProps) {
     if (typeof nextProps.auth !== 'boolean') {
       nextProps.checkAuth();
     }
     return null;
   }
+
   render() {
     return (
       <BrowserRouter>
         <div className={styles.App}>
           <Switch>
-            <Route path="/" exact component={routes.Index} />
-            <Route path="/demo" component={routes.Demo} />
-            <Route component={routes.NotFound} />
+            <Route path="/" exact component={routes.Index}/>
+            <Route path="/demo" component={routes.Demo}/>
+            <Route component={routes.NotFound}/>
           </Switch>
         </div>
       </BrowserRouter>
